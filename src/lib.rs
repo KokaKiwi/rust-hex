d701e963c21978ac8d083fc5350a2efdec8bf4d3
KokaKiwi <kokakiwi@kokakiwi.net>
2016-04-09 02:16:47 +0900

    Update license texts to add the Rust Project developers. Fix #7

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+// Copyright (c) 2013-2014 The Rust Project Developers.
+// Copyright (c) 2015-2016 The rust-hex Developers.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use std::error;
 use std::fmt;
 
